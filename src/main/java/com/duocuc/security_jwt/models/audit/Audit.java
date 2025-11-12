09e0b018e5d2f6f2a93ea3e98ff882d8d363d36a
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-11-12 09:16:27 -0300

    Se crea la clase audit

---
+++ src/main/java/com/duocuc/security_jwt/models/audit/Audit.java
@@ -0,0 +1,32 @@
+package com.duocuc.security_jwt.models.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Embeddable
+public class Audit {
+    // Variables Pre
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
